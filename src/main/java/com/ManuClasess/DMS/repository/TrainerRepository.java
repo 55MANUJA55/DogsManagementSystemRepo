5c19630cb9e13b4b2929b3c2748bc9902a4f7340
55MANUJA55 <manujaprasadam@gmail.com>
2024-04-18 15:09:02 -0500

    ADDED

---
+++ src/main/java/com/ManuClasess/DMS/repository/TrainerRepository.java
@@ -0,0 +1,9 @@
+package com.ManuClasess.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ManuClasess.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
