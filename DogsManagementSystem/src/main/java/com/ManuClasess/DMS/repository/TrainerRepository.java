f86344009a9dec2180df4ca621e37990ec9de1c5
Manuja Prasadam <143057264+55MANUJA55@users.noreply.github.com>
2024-04-18 14:40:20 -0500

    Delete DogsManagementSystem directory

--- DogsManagementSystem/src/main/java/com/ManuClasess/DMS/repository/TrainerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ManuClasess.DMS.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ManuClasess.DMS.Models.Trainer;
-
-public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
-
-}
